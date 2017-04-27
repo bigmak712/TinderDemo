2c19afa7cec99ca634c8be9f018cc51196a27687
bigmak712 <bigmak712@gmail.com>
2017-04-26 18:51:49 -0700

    Kind of having it working...need to fix rotation

--- TinderDemo/ViewController.swift
+++ TinderDemo/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardImageView: UIImageView!
+    
+    var newlyCreatedCard: UIImageView!
+    var cardInitialCenter: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didDragCard(_ sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translation(in: view)
+        let x = translation.x
+        
+        if sender.state == .began {
+            print("began")
+            newlyCreatedCard = sender.view as! UIImageView
+            cardInitialCenter = newlyCreatedCard.center
 
+        }
+        else if sender.state == .changed {
+            print("changed")
+            
+            if(x > 0) {
+                 newlyCreatedCard.transform = newlyCreatedCard.transform.rotated(by: CGFloat(2 * Double.pi / 180))
+            }
+            else {
+                newlyCreatedCard.transform = newlyCreatedCard.transform.rotated(by: CGFloat(-2 * Double.pi / 180))
+            }
+
+        }
+        else if sender.state == .ended {
+            if x > 50 {
+                UIView.animate(withDuration: 0.1, animations: {
+                    self.newlyCreatedCard.alpha = 0
+                })
+            }
+            else if x < -50 {
+                UIView.animate(withDuration: 0.1, animations: {
+                    self.newlyCreatedCard.alpha = 0
+                })
+            }
+            else {
+                newlyCreatedCard.transform = .identity
+            }
+        }
+    }
 }
 
